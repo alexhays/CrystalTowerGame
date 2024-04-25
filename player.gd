5cdb16a2f0294a624e7de1e43ed43a10a2d30c4c
alexhays <60071721+alexhays@users.noreply.github.com>
2024-04-24 21:53:06 -0500

    Adjusted camera controls
    Modified camera to make it feel nicer

--- player.gd
+++ player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
 
-const SPEED = 600.0
-const MAX_ZOOM = 2
-const MIN_ZOOM = 1
+@export var SPEED = 1000.0 # speed of camera
+@export var ZOOM_SLOWDOWN = 200.0 # camera slowdown with zoom
+@export var MAX_ZOOM = 3
+@export var MIN_ZOOM = 0.5
+@export var ZOOM_AMOUNT = 0.2
 
 
 func _physics_process(delta):
@@ -20,6 +22,14 @@ func _physics_process(delta):
 		direction.y = directionY
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
-	velocity = direction.normalized()*SPEED
+	velocity = direction.normalized()*(SPEED-ZOOM_SLOWDOWN*$Camera2D.zoom.x)
 
 	move_and_slide()
+
+func _input(event):
+	if event.is_action_released("zoomIn"):
+		$Camera2D.zoom.x = clamp($Camera2D.zoom.x+ZOOM_AMOUNT,MIN_ZOOM,MAX_ZOOM)
+		$Camera2D.zoom.y = clamp($Camera2D.zoom.y+ZOOM_AMOUNT,MIN_ZOOM,MAX_ZOOM)
+	if event.is_action_released("zoomOut"):
+		$Camera2D.zoom.x = clamp($Camera2D.zoom.x-ZOOM_AMOUNT,MIN_ZOOM,MAX_ZOOM)
+		$Camera2D.zoom.y = clamp($Camera2D.zoom.y-ZOOM_AMOUNT,MIN_ZOOM,MAX_ZOOM)
