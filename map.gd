4a68412fae9e6ffecd2c18fa4ec26ee0ff409626
alexhays <60071721+alexhays@users.noreply.github.com>
2024-04-28 08:04:50 -0500

    Spike Tower Edition (#3)
    * Spike Tower Edition
    
    Added Spikes and functionality to spawn/remove them. Added global scale for easy debugging.
    
    * World Border and Camera Control
    
    Added a world border and movable camera
    
    * Adjusted camera controls
    
    Modified camera to make it feel nicer
    
    * Added Variable grass type
    
    can only place towers on grass
    
    * Add Wall and UI for tower selection
    
    Allow more than just one tower and setup ui for the selection

--- map.gd
+++ map.gd
@@ -1,5 +1,8 @@
 extends TileMap
 
+signal leftClick(pos)
+signal rightClick(pos)
+
 var tile = Vector2(0,0)
 
 # Called when the node enters the scene tree for the first time.
@@ -11,4 +14,10 @@ func _ready():
 func _process(delta):
 	erase_cell(2,tile)
 	tile = local_to_map(get_local_mouse_position())
-	set_cell(2,tile,1,Vector2(0,0),0)
+	if get_cell_source_id(0,tile)==0:
+		set_cell(2,tile,1,Vector2(0,0),0)
+		var globalPos = to_global(map_to_local(tile))
+		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+			emit_signal("leftClick",globalPos.x,globalPos.y)
+		elif Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+			emit_signal("rightClick",globalPos.x,globalPos.y)
