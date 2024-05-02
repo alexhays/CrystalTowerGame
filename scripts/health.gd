52c6a5de34e4dadf0ccdfe05c524e864decbfff9
alexhays <60071721+alexhays@users.noreply.github.com>
2024-05-01 19:28:21 -0500

    Fix Health and make font dynamic

--- scripts/health.gd
+++ scripts/health.gd
@@ -2,9 +2,12 @@ class_name Health
 extends Node
 
 @export var max_health:float = 100
-@export var min_health:float = 100
+@export var min_health:float = 0
 @export var health_step:float = 0.01
 @export var value:float = 100
+
+func _ready():
+	value = format(value)
 
 func format(num:float):
 	return snappedf(clamp(num,min_health,max_health),health_step)
--- scripts/health_bar.gd
+++ scripts/health_bar.gd
@@ -6,13 +6,16 @@ var label:Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#assert(health!=null,"Missing Required Health Component")
+	assert(health!=null,"Missing Required Health Component")
 	label = Label.new()
 	add_child(label)
 	label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
 	label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
 	label.size.x = size.x
 	label.size.y = size.y
+	var fontSize = get_theme_font_size("font_size")
+	label.add_theme_font_size_override("font_size",fontSize)
+	size.y = label.size.y
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
