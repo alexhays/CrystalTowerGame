7ecf436e4e852c0a9b06cdc6388cb59b23576d76
alexhays <60071721+alexhays@users.noreply.github.com>
2024-05-01 20:04:49 -0500

    Switched to using timer queue

--- scripts/spike_tower.gd
+++ scripts/spike_tower.gd
@@ -5,10 +5,11 @@ var timer:Timer = Timer.new()
 
 func _ready():
 	# timer for repeatable events
-	timer.one_shot = true
-	timer.wait_time = 0.1
+	timer.one_shot = false
+	timer.wait_time = 0.5
 	add_child(timer)
 	timer.start()
+	timer.timeout.connect(_timer_queue)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,6 +17,7 @@ func _process(delta):
 	$Spike.rotate(delta*speed)
 	if $Health.value <= $Health.min_health:
 		queue_free()
-	if timer.is_stopped():
-		$Health.sub(1)
-		timer.start()
+
+func _timer_queue():
+	# test health decrease
+	$Health.sub(1)
--- scripts/wall_tower.gd
+++ scripts/wall_tower.gd
@@ -5,14 +5,16 @@ var timer:Timer = Timer.new()
 
 func _ready():
 	# timer for repeatable events
-	timer.one_shot = true
-	timer.wait_time = 0.1
+	timer.one_shot = false
+	timer.wait_time = 0.5
 	add_child(timer)
 	timer.start()
+	timer.timeout.connect(_timer_queue)
 
 func _process(delta):
 	if $Health.value <= $Health.min_health:
 		queue_free()
-	if timer.is_stopped():
-		$Health.sub(1)
-		timer.start()
+
+func _timer_queue():
+	# test health decrease
+	$Health.sub(1)
